713d3140a699d289d2aae62292308391a878f8bf
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-02-18 00:15:57 +0700

    feat: moved solveBoard process to SolverTask

--- src/main/java/stima/modules/Solver.java
+++ src/main/java/stima/modules/Solver.java
@@ -4,43 +4,19 @@ public class Solver {
     public int selX, selY;
     public Board currBoard;
     public int iterations;
+    public String processState;
+    public long timeElapsed;
 
     public Solver(Board B) {
         this.currBoard = B;
         this.selX = 0;
         this.selY = 0;
         this.iterations = 0;
+        this.processState = "";
+        this.timeElapsed = 0;
     }
 
-
-    public void solveBoard() {
-        long start = System.currentTimeMillis();
-        while(!this.noPossibilityLeft()) {
-            while(!this.noTilesLeft()) {
-                if (this.isQueenPlaceableAtSelected()) {
-                    this.placeQueenAtSelected();
-                    this.setSelectedTile(0, 0);
-                    if (allQueensPlaced()) break;
-                }
-                this.moveSelectedTile();
-            }
-            if (allQueensPlaced()) break;
-            this.backtrackToLastQueen();
-        }
-        if (allQueensPlaced()) {
-            this.iterations++;
-            long finish = System.currentTimeMillis();
-            this.currBoard.printBoard();
-            System.out.println("Time elapsed : " + (finish - start) + "ms");
-            System.out.println("Total iterations : " + this.iterations);
-
-        }
-        if (this.noPossibilityLeft()) {
-            System.out.println("Not Possible");
-        }
-    }
-
-    private void moveSelectedTile() {
+    public void moveSelectedTile() {
         this.selX++;
         if (this.selX >= this.currBoard.boardDimension) {
             this.selX = 0;
@@ -48,28 +24,28 @@ public class Solver {
         }
     }
 
-    private void setSelectedTile(int x, int y) {
+    public void setSelectedTile(int x, int y) {
         this.selX = x;
         this.selY = y;
     }
 
-    private boolean noTilesLeft() {
+    public boolean noTilesLeft() {
         return this.selY >= this.currBoard.boardDimension;
     }
 
-    private boolean allQueensPlaced() {
+    public boolean allQueensPlaced() {
         return this.currBoard.queenAmount == this.currBoard.boardDimension;
     }
 
-    private boolean noPossibilityLeft() {
+    public boolean noPossibilityLeft() {
         return this.noTilesLeft() && this.currBoard.queenAmount == 0;
     }
 
-    private boolean isQueenPlaceableAtSelected() {
+    public boolean isQueenPlaceableAtSelected() {
         return this.currBoard.isQueenPlaceableAt(this.selX, this.selY);
     }
 
-    private void placeQueenAtSelected() {
+    public void placeQueenAtSelected() {
         this.currBoard.placeQueen(this.selX, this.selY);
         // for (int i = 0; i < this.currBoard.queenAmount; i++) {
         //     System.out.print("  ");
@@ -77,7 +53,7 @@ public class Solver {
         //System.out.println("Placed Queen " + this.currBoard.queenAmount + " at (" + this.selX + ", " + this.selY + ")");
     }
 
-    private void backtrackToLastQueen() {
+    public void backtrackToLastQueen() {
         Tile prevQueenTile = this.currBoard.lastQueenTile();
         this.currBoard.removeLastQueen();
         this.setSelectedTile(prevQueenTile.x, prevQueenTile.y);
