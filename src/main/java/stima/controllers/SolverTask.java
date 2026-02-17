4d1613e91574fcaf8cacf01522dfc1931a39e0ce
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-02-18 00:15:21 +0700

    feat: added SolverTask for responsive GUI

---
+++ src/main/java/stima/controllers/SolverTask.java
@@ -0,0 +1,43 @@
+package stima.controllers;
+
+import javafx.concurrent.Task;
+
+import stima.modules.*;
+
+public class SolverTask extends Task<Integer>{
+
+    private Solver solver;
+    
+    public SolverTask(Solver S) {
+        this.solver = S;
+    }
+
+    @Override
+    protected Integer call() throws Exception {
+        long start = System.currentTimeMillis();
+       solver.processState = "Processing...";
+        while(!solver.noPossibilityLeft()) {
+            while(!solver.noTilesLeft()) {
+                if (solver.isQueenPlaceableAtSelected()) {
+                    solver.placeQueenAtSelected();
+                    solver.setSelectedTile(0, 0);
+                    if (solver.allQueensPlaced()) break;
+                }
+                solver.moveSelectedTile();
+            }
+            if (solver.allQueensPlaced()) break;
+            solver.backtrackToLastQueen();
+            solver.timeElapsed = (System.currentTimeMillis() - start);
+            updateValue(solver.iterations);
+        }
+        if (solver.allQueensPlaced()) {
+            solver.iterations++;
+            solver.processState = "Answer Found";
+        }
+        if (solver.noPossibilityLeft()) {
+            solver.processState = "Answer not Found";
+        }
+        return solver.iterations;
+    }
+    
+}
